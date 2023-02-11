ab997153d3b5153d10b066a5363840896fb838df
Erwin Castro <90945345+ecastro159@users.noreply.github.com>
2023-02-11 15:49:51 -0500

    Creating Models and Adding Connection Properties

---
+++ project4/src/main/java/com/cognixia/jump/project4/models/BankAccount.java
@@ -0,0 +1,93 @@
+package com.cognixia.jump.project4.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "BankAccount")
+public class BankAccount{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer accountNumber;
+
+    @Column(name = "customer_id", nullable = false)
+    private Integer customerID;
+
+
+    @Column(columnDefinition = "double default 0" ,name= "checking_account")
+    private double checkingAccount;
+
+    @Column(columnDefinition = "double default 0", name = "savings_account")
+    private double savingsAccount;
+
+    // One to One Relation to Customer 
+    @ManyToOne
+    @JoinColumn(name = "customer_ID")
+    private Customer customer;
+
+    public BankAccount(Integer accountNumber, Integer customerID, double checkingAccount, double savingsAccount,
+            Customer customer) {
+        this.accountNumber = accountNumber;
+        this.customerID = customerID;
+        this.checkingAccount = checkingAccount;
+        this.savingsAccount = savingsAccount;
+        this.customer = customer;
+    }
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Integer getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Integer customerID) {
+        this.customerID = customerID;
+    }
+
+    public double getCheckingAccount() {
+        return checkingAccount;
+    }
+
+    public void setCheckingAccount(double checkingAccount) {
+        this.checkingAccount = checkingAccount;
+    }
+
+    public double getSavingsAccount() {
+        return savingsAccount;
+    }
+
+    public void setSavingsAccount(double savingsAccount) {
+        this.savingsAccount = savingsAccount;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount [accountNumber=" + accountNumber + ", customerID=" + customerID + ", checkingAccount="
+                + checkingAccount + ", savingsAccount=" + savingsAccount + ", customer=" + customer + "]";
+    }
+
+    
+
+}
--- project4/src/main/java/com/cognixia/jump/project4/models/Customer.java
+++ project4/src/main/java/com/cognixia/jump/project4/models/Customer.java
@@ -1,33 +1,102 @@
 package com.cognixia.jump.project4.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
 
 // Simple Customer Model for new customers
 
 @Entity
+@Table(name = "Customer")
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_ID")
     private Integer customerID;
 
-    @Column(name = "first_name", nullable=false)
+    @Column(name = "first_name", columnDefinition = "varchar(50)", nullable = false)
     private String firstName;
 
-
-    @Column(name = "last_name", nullable=false)
+    @Column(name = "last_name", columnDefinition = "varchar(50)", nullable = false)
     private String lastName;
 
-    @Column(name = "contact_number", nullable = false)
+    @Column(name = "contact_number", columnDefinition = "varchar(50)", nullable = false)
     private String contactInfo;
 
-    @Column(name = "user_password", nullable = false)
+    @Column(name = "user_password", columnDefinition = "varchar(10)", nullable = false)
     private String password;
 
+    // One to One 
+    @OneToMany(mappedBy = "customer")
+    private BankAccount bankAccount;
+
+    public Customer() {
+    }
+
+    public Customer(Integer customerID, String firstName, String lastName, String contactInfo, String password) {
+        super();
+        this.customerID = customerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactInfo = contactInfo;
+        this.password = password;
+    }
+
+    public Integer getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Integer customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [customerID=" + customerID + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", contactInfo=" + contactInfo + ", password=" + password + "]";
+    };
 
     
+
 }
