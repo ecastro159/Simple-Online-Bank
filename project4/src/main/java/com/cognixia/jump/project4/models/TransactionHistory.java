0d1744d0077a85fd06399f0441b1eab1510634c1
Erwin Castro <90945345+ecastro159@users.noreply.github.com>
2023-02-13 10:09:12 -0500

    added setters and getters and constructors for Transaction class

--- project4/src/main/java/com/cognixia/jump/project4/models/TransactionHistory.java
+++ project4/src/main/java/com/cognixia/jump/project4/models/TransactionHistory.java
@@ -27,5 +27,63 @@ public class TransactionHistory {
     @Column
     private LocalDateTime accessTime;
 
+    
+
+    public TransactionHistory() {
+    }
+
+    
+
+    public TransactionHistory(Integer transactionID, Integer accountNumber, Integer customerID, String transactionType,
+            double amount, LocalDateTime accessTime) {
+        this.transactionID = transactionID;
+        this.accountNumber = accountNumber;
+        this.customerID = customerID;
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.accessTime = accessTime;
+    }
+
+
+
+    public Integer getTransactionID() {
+        return transactionID;
+    }
+    public void setTransactionID(Integer transactionID) {
+        this.transactionID = transactionID;
+    }
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public Integer getCustomerID() {
+        return customerID;
+    }
+    public void setCustomerID(Integer customerID) {
+        this.customerID = customerID;
+    }
+    public String getTransactionType() {
+        return transactionType;
+    }
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public LocalDateTime getAccessTime() {
+        return accessTime;
+    }
+    public void setAccessTime(LocalDateTime accessTime) {
+        this.accessTime = accessTime;
+    }
+
+    
+
 
 }
